b6ffd0dd484edebaca07740438a5d0a366ce1dd4
lasags <lasags@naver.com>
2022-10-20 03:14:54 +0900

    UPDATE layout About, Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,45 +15,45 @@ const Skills = () => {
       {/* Container */}
       <div className=" jua max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p className="pb-28 md:pb-0"></p>
-          <p className="px-4 text-4xl font-bold ">Skills</p>
-          <p className="px-4 py-6">
+          <p className="pb-20 md:pb-0"></p>
+          <p className=" text-3xl md:text-4xl text-center md:text-start px-4 md:px-12 font-bold ">Skills</p>
+          <p className=" text-sm md:text-base text-center md:text-start px-4 md:px-12 py-1 md:py-4">
             These are the technologies I've worked with
           </p>
         </div>
 
-        <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-4 text-center py-2">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-12 md:w-20 mx-auto pt-6" src={HTML} alt="HTML icon" />
-            <p className="my-4">HTML</p>
+        <div className="w-[70%]  md:w-[90%] xl:w-full  grid grid-cols-2 md:grid-cols-4 gap-4 mx-auto text-center py-2">
+          <div className=" shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-10 sm:w-12 md:w-20 mx-auto pt-6" src={HTML} alt="HTML icon" />
+            <p className=" text-xs sm:text-sm md:text-base my-[0.35rem] sm:my-2 md:my-4">HTML</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-12 md:w-20 mx-auto pt-6" src={CSS} alt="HTML icon" />
-            <p className="my-4">CSS</p>
+            <img className="w-10 sm:w-12 md:w-20 mx-auto pt-6" src={CSS} alt="HTML icon" />
+            <p className=" text-xs sm:text-sm md:text-base my-[0.35rem] sm:my-2 md:my-4">CSS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-12 md:w-20 mx-auto pt-6" src={JavaScript} alt="HTML icon" />
-            <p className="my-4">JAVASCRIPT</p>
+            <img className="w-10 sm:w-12 md:w-20 mx-auto pt-6" src={JavaScript} alt="HTML icon" />
+            <p className=" text-xs sm:text-sm md:text-base my-[0.35rem] sm:my-2 md:my-4">JAVASCRIPT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-12 md:w-20 mx-auto pt-6" src={ReactImg} alt="HTML icon" />
-            <p className="my-4">REACT</p>
+            <img className="w-10 sm:w-12 md:w-20 mx-auto pt-6" src={ReactImg} alt="HTML icon" />
+            <p className=" text-xs sm:text-sm md:text-base my-[0.35rem] sm:my-2 md:my-4">REACT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-12 md:w-20 mx-auto pt-6" src={Node} alt="HTML icon" />
-            <p className="my-4">NODE JS</p>
+            <img className="w-10 sm:w-12 md:w-20 mx-auto pt-6" src={Node} alt="HTML icon" />
+            <p className=" text-xs sm:text-sm md:text-base my-[0.35rem] sm:my-2 md:my-4">NODE JS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-12 md:w-20 mx-auto pt-6" src={Mongo} alt="HTML icon" />
-            <p className="my-4">MONGO DB</p>
+            <img className="w-10 sm:w-12 md:w-20 mx-auto pt-6" src={Mongo} alt="HTML icon" />
+            <p className=" text-xs sm:text-sm md:text-base my-[0.35rem] sm:my-2 md:my-4">MONGO DB</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-12 md:w-20 mx-auto pt-6" src={Tailwind} alt="HTML icon" />
-            <p className="my-4">TAILWIND</p>
+            <img className="w-10 sm:w-12 md:w-20 mx-auto pt-6" src={Tailwind} alt="HTML icon" />
+            <p className=" text-xs sm:text-sm md:text-base my-[0.35rem] sm:my-2 md:my-4">TAILWIND</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-12 md:w-20 mx-auto pt-6" src={GitHub} alt="HTML icon" />
-            <p className="my-4">GITHUB</p>
+            <img className="w-10 sm:w-12 md:w-20 mx-auto pt-6" src={GitHub} alt="HTML icon" />
+            <p className=" text-xs sm:text-sm md:text-base my-[0.35rem] sm:my-2 md:my-4">GITHUB</p>
           </div>
         </div>
       </div>
