2c857ba0ac79042370d78628cbc194f3be59b2ae
lasags <lasags@naver.com>
2022-10-21 20:39:31 +0900

    ADD minimatch >= 3.05 to package.json

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import profileimg from '../assets/profile_img.png';
 const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-white">
-      <div className="flex flex-col justify-center items-center w-full h-full ">
+      <div className=" flex flex-col justify-center items-center w-full h-full ">
         <div className="max-w-[620px] md:max-w-[1250px] w-full grid grid-cols-1 gap-8 mt-32 md:mt-20">
           <div className=" nanum font-bold mx-auto mb-6 text-3xl md:text-5xl md:pt-0">
             About Me
@@ -37,7 +37,7 @@ const About = () => {
             소대장 복무 경험을 통해  <strong>주도적이고 책임감 있게 주어진 업무를 해결</strong>합니다.
             </p>
             <p className=' mb-16 text-xs sm:text-sm lg:text-xl md:mb-0'>
-            파이널 프로젝트에서 부트캠프 과정 중 학습했던 MySQL이 아닌 MongoDB를 도입하는 등 <strong>새로운 기술을 도입에 적극적</strong>입니다.
+            파이널 프로젝트에서 부트캠프 과정 중 집중적으로 학습했던 MySQL이 아닌 MongoDB를 도입하는 등 <strong>새로운 기술을 도입에 적극적</strong>입니다.
             </p>
           </div>
         </div>
